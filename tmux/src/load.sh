4e604b2add2dc67ae7ece5f9dc84a7cd5650751c
mohammadne <mohammadne.dev@gmail.com>
2022-08-08 21:20:29 +0430

    faet: add loader

---
+++ tmux/src/load.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+style=$1
+theme=$2
+
+# load color palette
+source ~/.config/tmux/src/themes/"$theme".conf
+
+function set() {
+   tmux set-option -gq "$1" "$2"
+}
+
+function setw() {
+   tmux set-window-option -gq "$1" "$2"
+}
+
+# message infos
+set "message-style"         "fg=foreground,bg=background,bold" # status line messages style
+set "message-command-style" "fg=foreground,bg=background,bold" # in command mode with vi keys
+
+# clock configuration ( prefix + t )
+setw "clock-mode-colour" "$active"
+setw "clock-mode-style " "24"
+
+# set inactive/active pane border style
+set "pane-active-border-style" "fg=$active"
+set "pane-border-style"        "fg=$inactive"
+
+# set inactive/active (number) panes styles ( prefix + q )
+set "display-panes-active-colour" "$active"
+set "display-panes-colour" "$inactive"
+
+# main style of each pane
+set "window-active-style" "fg=$foreground,bg=default"
+set "window-style" "fg=$inactive,bg=default"
+
+# inactive (empty space) status style
+set "status-style" "fg=$background,bg=$background"
+
+# source the style with appropriate theme
+source ~/.config/tmux/src/styles/"$style".sh "$theme"
