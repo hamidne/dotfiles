a0bbdb703735f859614bad8f6c6584a89f739652
mohammadne <mohammadne.dev@gmail.com>
2022-08-08 21:24:20 +0430

    feat: add angled style

---
+++ tmux/themes/styles/angled.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# load color palette config
+source ~/.tmux/themes/configs/"$1".conf
+
+function set() {
+   tmux set-option -gq "$1" "$2"
+}
+
+function setw() {
+   tmux set-window-option -gq "$1" "$2"
+}
+
+# active left status style
+set "status-left" "\
+#[fg=$edge_fg,bg=$edge_bg,bold] #S \
+#[fg=$edge_bg,bg=$background]"
+
+# prefix highlight when pressed
+set "@prefix_highlight_fg" "$hover_fg"
+set "@prefix_highlight_bg" "$hover_bg"
+set "@prefix_highlight_prefix_prompt" "prefix"
+set "@prefix_highlight_empty_prompt" ""
+set "@prefix_highlight_output_prefix" ""
+set "@prefix_highlight_output_suffix" " "
+
+# # flags for various options
+# FLAGS=" \
+# #[fg=colour7]\
+# #{?client_prefix,<prefix> ,}\
+# #[fg=colour3]\
+# #{s/-mode/ /:pane_mode}\
+# #[fg=colour6]\
+# #{?pane_synchronized,sync ,}\
+# #[fg=colour4]\
+# #{?pane_marked,mark ,}\
+# #[fg=colour7]\
+# #{?#{&&:#{pane_marked_set},#{!=:#{pane_marked},1}},mark ,}\
+# #{?rectangle_toggle,rect ,}\
+# #{?selection_present,sel ,}\
+# #{?pane_dead,dead ,}\
+# #{?window_zoomed_flag,zoom ,}\
+# #{?window_linked,link ,}\
+# #{?session_many_attached,attach ,}\
+# #{?#{&&:#{session_grouped},#{!=:#{session_group_size},1}},group ,}\
+# #[fg=colour5]\
+# #{?client_readonly,readonly ,}"
+
+# active right status style
+set "status-right" "\
+#[fg=$hover_bg,bg=$background]\
+#{prefix_highlight}\
+#[fg=$hover_fg,bg=$hover_bg,nounderscore,noitalics] %H:%M  %a \
+#[fg=$background,bg=$hover_bg]\
+#[fg=$edge_bg,bg=$background]\
+#[fg=$edge_fg,bg=$edge_bg,bold] #{USER}@#h "
+
+# inactive status windows style
+set "window-status-format" "\
+#[fg=$background,bg=$background]\
+#[fg=$foreground,bg=$background] #I  #W \
+#[fg=$background,bg=$background]"
+
+# active status windows style
+set "window-status-current-format" "\
+#[fg=$background,bg=$hover_bg]\
+#[fg=$hover_fg,bg=$hover_bg,nobold] #I  #W \
+#[fg=$hover_bg,bg=$background]"
