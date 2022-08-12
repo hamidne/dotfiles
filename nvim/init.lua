c4d8ec4759cba5073e34ed54d7c34db9f48e221d
mohammadne <mohammadne@mail.ru>
2022-08-12 20:35:07 +0430

    update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,11 +2,8 @@
 -- ]]
 
 --  NOTE: core Must happen before plugins are required
-require('core.mappings') -- key maps, binding configurations
-require('core.options') -- nvim options and configuration
+require('core.mappings') -- keymaps binding configurations
+require('core.options') -- neovim options
 
 -- in case we are loading the plugins, we return and don't setup them
-if require('plugins.init') then return end
-
-require('plugins.config.statusline') -- Plugins
-require('plugins.config.colorscheme') -- Plugins
+require('plugins')
