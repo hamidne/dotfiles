5b8fe15b3627dc68a817387df958d053bc64e72d
mohammadne <mohammadne@mail.ru>
2022-08-12 20:36:10 +0430

    add git icons

---
+++ nvim/lua/icons/git.lua
@@ -0,0 +1,15 @@
+return {
+    logo = "",
+    unstaged = "",
+    staged = "S",
+    unmerged = "",
+    renamed = "",
+    untracked = "",
+    deleted = "",
+    ignored = "",
+}
+
+-- Diff = "",
+-- Repo = "",
+-- Octoface = "",
+
