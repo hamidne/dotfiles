bb7cf8a4d750808144c0dd59638cde601e3a8891
mohammadne <mohammadne@mail.ru>
2022-08-12 20:36:29 +0430

    add folder icons

---
+++ nvim/lua/icons/folder.lua
@@ -0,0 +1,10 @@
+return {
+    arrow_closed = "",
+    arrow_open = "",
+    default = "",
+    open = "",
+    empty = "",
+    empty_open = "",
+    symlink = "",
+    symlink_open = "",
+}
