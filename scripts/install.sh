c13c4bc83e3e02b71f1aca14f9d9838a50c47df2
mohammadne <mohammadne@mail.ru>
2022-08-14 12:17:02 +0430

    feat: update vim & nvim

--- scripts/install.sh
+++ scripts/install.sh
@@ -118,14 +118,15 @@ quiet_git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # -------------------------------------------------------------------------- VIM
 
-echo "Installing & Activating vim plug plugin manager ..."
+echo "Installing & Activating vim-plug ..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qa || echo "Something went wrong installing Neovim plugins."
+vim +PlugInstall +qa || echo "Something went wrong installing vim plugins."
 
 # -------------------------------------------------------------------------- NeoVIM
 
-echo "Installing & Activating nvim packer plugin manager ..."
+echo "Installing & Activating packer ..."
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim +PackerSync +qa || echo "Something went wrong installing nvim plugins."
 
 # -------------------------------------------------------------------------- Fonts
 
