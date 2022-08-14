20ecb3b8e001f8efb7f16cdfe18a178d5a2d9794
mohammadne <mohammadne@mail.ru>
2022-08-14 15:12:44 +0430

    feat: update linker

--- scripts/linker.sh
+++ scripts/linker.sh
@@ -16,4 +16,22 @@ if [ ! -d "$dotfiles_home" ]; then
     git clone https://github.com/mohammadne/dotfiles.git $dotfiles_home --quiet
 fi
 
-echo 'test'
+# git
+ln -s "$dotfiles_home/git" ~/.git
+
+# zsh related
+ln -s "$dotfiles_home/zsh/zshrc" ~/.zshrc
+ln -s "$dotfiles_home/zsh/zsh.aliases" ~/.zsh.aliases
+ln -s "$dotfiles_home/zsh/p10k.zsh" ~/.config/p10k.zsh
+
+# tmux related
+ln -s "$dotfiles_home/tmux/themes" ~/.tmux/themes
+ln -s "$dotfiles_home/tmux/tmux.conf" ~/.tmux.conf
+
+# vim
+ln -s "$dotfiles_home/vim/vimrc" ~/.vimrc
+
+# nvim
+ln -s "$dotfiles_home/nvim" ~/.config/nvim
+
+
